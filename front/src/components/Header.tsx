05ee15810ac5b6075440a056b627ed6a59358dc8
David Natan <rdpalominop@ucvvirtual.edu.pe>
2024-12-28 23:21:01 +0100

    Ingenius

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header:React.FC = () => {
   return (
     <div className='z-10'>
       <h1 className='
-      font-roboto flex flex-row justify-center text-[#EDEDED] text-[1.2rem] font-bold 
+      font-roboto flex flex-row justify-center text-[#CBCACA] text-[1.2rem] font-bold 
       sm:text-[2.5rem]
       ' >TAKE THE INGENIUS CONTROL</h1>
     </div>
